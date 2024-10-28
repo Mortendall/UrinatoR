0f45f623b5ca2ff663efe0602b8c352a29e414cb
Morten Dall <mortendall1988@gmail.com>
2024-10-28 15:20:41 +0100

    collected data processing in functions

--- R/dataUpload.R
+++ R/dataUpload.R
@@ -43,74 +43,16 @@ upload <- function(id, data, parentSession){
        #Press 'process data' to proceed
 
        shiny::observeEvent(input$fileUpload,{
-           datafile <- input$fileUpload
-           ext <- tools::file_ext(datafile$datapath)
-           req(datafile)
-           validate(need(ext == "csv", "Please upload a csv file"))
-           data$rawData <- vroom::vroom(datafile$datapath,
-                                        show_col_types = F,
-                                        delim = input$seperator,
-                                        locale = vroom::locale(decimal_mark = input$decimal)
-                                        )
-
-           #write in check of columns if to check if right format has been submitted
-
-           #extract group names from column headers
-
-           testGroups <- stringr::str_subset(colnames(data$rawData),
-                                             pattern = "_TIMESTAMP$")
-           data$groups <- stringr::str_replace(testGroups,
-                                              pattern = "_TIMESTAMP",
-                                              replacement = "")
-
-
-           #remove irrelevant columns
-           data$trimmedData<- data$rawData |>
-             dplyr::select(!tidyselect::ends_with(c("QRT",
-                                                    "AVG",
-                                                    "SEM",
-                                                    "SAMPLES")
-             )
-             )
-
-
-
-           #Data are time-stamped and vroom cannot handle that. We therefore
-           #import the data again for the first time stamp and extract the info
-           #we need
-
-           headername <- stringr::str_subset(colnames(data$rawData),
-                                             pattern = "_TIMESTAMP$")[1]
-
-           firsttimestamp <- vroom::vroom(datafile$datapath,
-                                          show_col_types = F,
-                                          delim = input$seperator,
-                                          locale = vroom::locale(decimal_mark = input$decimal),
-                                          col_select = headername,
-                                          col_types = vroom::cols(.default = "c"))
-
-           timeadjust <- stringr::str_extract(firsttimestamp[1,1],
-                                              pattern = "[+-]?[:digit:]{4}$")
-
-
-
-           timeadjustTime <- lubridate::hm(paste0(stringr::str_extract(timeadjust,
-                                                                      pattern = "(?<=[+-])[:digit:]{2}"),
-                                                  ":",
-                                                 stringr::str_extract(timeadjust,
-                                                                      pattern = "[:digit:]{2}$")))
-
-           timeadjustFactor <- as.numeric(timeadjustTime)/3600
-           #check if the timestamp factor is positive or negative
-           if(isTRUE(stringr::str_extract(timeadjust,
-                                   pattern = "^[+-]{1}")=="+")){
-             data$trimmedData <- data$trimmedData |>
-               dplyr::mutate(hour = hour + timeadjustFactor)
-           }
-           else{
-             data$trimmedData <- data$trimmedData |>
-               dplyr::mutate(hour = hour - timeadjustFactor)
-           }
+            data$rawData <- load_data_file(input$fileUpload,
+                                           input$separator,
+                                           input$decimal)
+            data$groups <- extract_group_info(data$rawData)
+            data$trimmedData <- process_data(data$rawData,
+                                             input$fileUpload,
+                                             data$groups)
+            #prepare group data
+            data$groupinfo <- data.frame("CageID"= names(data$trimmedData),
+                                         "No.ofAnimals" = 1)
          }
          )
       output$Table<-  shiny::renderUI({
@@ -118,21 +60,15 @@ upload <- function(id, data, parentSession){
         shiny::tagList(
 
                     shiny::textOutput(outputId = ns("groups"))
-                    #,
-                    #rhandsontable::rHandsontableOutput(outputId = ns("dataScreen"))
+
           )
       })
       output$groups <- shiny::renderText({
         paste("The following groups have been detected in the dataset: ",
               paste(data$groups, collapse = ", "))
       })
-      #original version included table of data, but this did not contribute much.
-      #I have therefore commented it out from being rendered
-      output$dataScreen <- rhandsontable::renderRHandsontable({
-        req(data$trimmedData)
-        rhandsontable::rhandsontable(data$trimmedData)
-      })
 
+      #####Load demo data####
       shiny::observeEvent(input$demodata,{
         data$longData <- readRDS(here::here("Data/longData.rds"))
         data$circadiandata <- readRDS(here::here("Data/circadian.rds"))
@@ -176,8 +112,12 @@ upload <- function(id, data, parentSession){
       })
 
       #####Add event data####
+      shiny::observeEvent(input$eventUpload,{
+        data$events <- load_event_file(input$eventUpload,
+                                      input$eventSeparator,
+                                      input$eventDecimal)
 
-
+      })
 
       #####process data UI####
       #This code controls what happens when data process button is selected.
@@ -185,48 +125,48 @@ upload <- function(id, data, parentSession){
       shiny::observeEvent(input$ProcessData,{
         #prepare data in long format for plotting
 
-        TimeZero <- data$trimmedData$relativeTime[2]/3600
-        testdataLong <- data$trimmedData |>
-          #we remove the first row as this one does not represent a full hour,
-          #but when the animals were placed in the system
-          dplyr::slice(-1) |>
-          dplyr::select(- tidyselect::ends_with("TIMESTAMP"),
-                        #-day,
-                        -minute,
-                        - tidyselect::ends_with("SAMPLES")) |>
-          dplyr::mutate(TimeElapsed = relativeTime/3600-TimeZero) |>
-          dplyr::select(-relativeTime) |>
-          tidyr::pivot_longer(cols = - c(TimeElapsed, hour, day),
-                              names_to = "ID",
-                              values_to = "Rawdata") |>
-          dplyr::group_by(ID) |>
-          dplyr::arrange(TimeElapsed) |>
-          dplyr::mutate(Rawdata = as.numeric(stringr::str_replace_all(Rawdata, ",", "."))
-                        ,
-                         Value = Rawdata - dplyr::lead(Rawdata,
-                                            default = dplyr::last(Rawdata))
-                        )
+        # TimeZero <- data$trimmedData$relativeTime[2]/3600
+        # testdataLong <- data$trimmedData |>
+        #   #we remove the first row as this one does not represent a full hour,
+        #   #but when the animals were placed in the system
+        #   dplyr::slice(-1) |>
+        #   dplyr::select(- tidyselect::ends_with("TIMESTAMP"),
+        #                 #-day,
+        #                 -minute,
+        #                 - tidyselect::ends_with("SAMPLES")) |>
+        #   dplyr::mutate(TimeElapsed = relativeTime/3600-TimeZero) |>
+        #   dplyr::select(-relativeTime) |>
+        #   tidyr::pivot_longer(cols = - c(TimeElapsed, hour, day),
+        #                       names_to = "ID",
+        #                       values_to = "Rawdata") |>
+        #   dplyr::group_by(ID) |>
+        #   dplyr::arrange(TimeElapsed) |>
+        #   dplyr::mutate(Rawdata = as.numeric(stringr::str_replace_all(Rawdata, ",", "."))
+        #                 ,
+        #                  Value = Rawdata - dplyr::lead(Rawdata,
+        #                                     default = dplyr::last(Rawdata))
+        #                 )
 
         #calculate rolling mean and rolling sd
-        testdataLong <- testdataLong |>
-          dplyr::mutate(RollingSDpoint = zoo::rollapply(Rawdata,
-                                                        48,
-                                                        sd,
-                                                        fill = 0,
-                                                        partial = T,
-                                                        align = "left"),
-                        #originally include tested against abs(Value) instead of -Value
-                        #but this was changed to only detect sharp increases
-                         Include = dplyr::case_when(
-                           RollingSDpoint < -Value ~ 1,
-                           RollingSDpoint > -Value ~ 0
-                         ),
-                        CorrectedValue = dplyr::case_when(
-                          Include == 0 ~ Value,
-                          Include == 1 ~ NA
-                        )
-                        )|>
-          dplyr::rename(Individual= ID)
+        # testdataLong <- testdataLong |>
+        #   dplyr::mutate(RollingSDpoint = zoo::rollapply(Rawdata,
+        #                                                 48,
+        #                                                 sd,
+        #                                                 fill = 0,
+        #                                                 partial = T,
+        #                                                 align = "left"),
+        #                 #originally include tested against abs(Value) instead of -Value
+        #                 #but this was changed to only detect sharp increases
+        #                  Include = dplyr::case_when(
+        #                    RollingSDpoint < -Value ~ 1,
+        #                    RollingSDpoint > -Value ~ 0
+        #                  ),
+        #                 CorrectedValue = dplyr::case_when(
+        #                   Include == 0 ~ Value,
+        #                   Include == 1 ~ NA
+        #                 )
+        #                 )|>
+        #   dplyr::rename(Individual= ID)
 
 
         #function to add group based on ID
